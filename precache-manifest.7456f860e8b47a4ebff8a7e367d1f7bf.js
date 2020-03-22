038951193dce1ac7f476c1fd974055efc96a5e83
dayanandraut <dayanandraut2015@gmail.com>
2020-03-22 20:39:47 +0530

    Updates

---
+++ precache-manifest.7456f860e8b47a4ebff8a7e367d1f7bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "700d2ec0c1ee1defa9e2cdc4fdb8dcfb",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "9ad01de4b06f5307a8be",
+    "url": "/monster/static/css/main.ded7efa3.chunk.css"
+  },
+  {
+    "revision": "299f65059b0504546074",
+    "url": "/monster/static/js/2.eca8dab0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster/static/js/2.eca8dab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ad01de4b06f5307a8be",
+    "url": "/monster/static/js/main.e626c983.chunk.js"
+  },
+  {
+    "revision": "f26b0db8b91636d50869",
+    "url": "/monster/static/js/runtime-main.71f4129b.js"
+  }
+]);
